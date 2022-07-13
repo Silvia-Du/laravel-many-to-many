0416df6ba429671cd588e7616b78f36d5a0d13b6
Silvia-Du <silviadu@hotmail.it>
2022-07-13 10:03:08 +0200

    Popolato tabella Post_tag con dati random

---
+++ database/seeds/PostsTagsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Tag;
+use App\Post;
+
+class PostsTagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for( $i=0 ; $i< 25 ; $i++ ){
+            $post = Post::inRandomOrder()->first();
+            $tag_id = Tag::inRandomOrder()->first()->id;
+            $post->tags()->attach($tag_id);
+
+        }
+    }
+}
