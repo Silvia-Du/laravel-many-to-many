b6f2baeb860c5e2984fb6ab13eb170e7eb76e19d
Silvia-Du <silviadu@hotmail.it>
2022-07-13 09:26:49 +0200

    Create tabelle tags & ponte, popolato tabella tags

---
+++ database/migrations/2022_07_13_071204_create_post_tag_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            //col fk per i posts
+            $table->unsignedBigInteger('post_id');
+            //col fk per i tag
+            $table->unsignedBigInteger('tag_id');
+            //dico che sono fk
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
